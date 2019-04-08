d0e6f157b95d41eb9a8960f1b73979aaccad8f4b
sterling.walsh.dev@gmail.com <sterling.walsh.dev@gmail.com>
2019-04-08 13:53:38 -0400

    Updates

---
+++ precache-manifest.06aeed6e87042ca6f39269050be2e1cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/sterlingwalsh/robofriends/tree/gh-pages/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "9e01b228fd9319d277b7",
+    "url": "/sterlingwalsh/robofriends/tree/gh-pages/static/js/runtime~main.64dd4c67.js"
+  },
+  {
+    "revision": "8683cc33162b9b866ded",
+    "url": "/sterlingwalsh/robofriends/tree/gh-pages/static/js/main.0f0d04cb.chunk.js"
+  },
+  {
+    "revision": "23c9c903deda4738bb74",
+    "url": "/sterlingwalsh/robofriends/tree/gh-pages/static/js/2.01c51581.chunk.js"
+  },
+  {
+    "revision": "8683cc33162b9b866ded",
+    "url": "/sterlingwalsh/robofriends/tree/gh-pages/static/css/main.959aa37e.chunk.css"
+  },
+  {
+    "revision": "23c9c903deda4738bb74",
+    "url": "/sterlingwalsh/robofriends/tree/gh-pages/static/css/2.35136b2e.chunk.css"
+  },
+  {
+    "revision": "2701bc80818b1e9d3ab4fef883921f33",
+    "url": "/sterlingwalsh/robofriends/tree/gh-pages/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/robofriends/precache-manifest.bddda73ec7a775aed3e67539d4e0034b.js"
+  "/sterlingwalsh/robofriends/tree/gh-pages/precache-manifest.06aeed6e87042ca6f39269050be2e1cd.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/robofriends/index.html", {
+workbox.routing.registerNavigationRoute("/sterlingwalsh/robofriends/tree/gh-pages/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
