d42aecbdbcab08c4e141b1e0560fe96f267db75a
sterling.walsh.dev@gmail.com <sterling.walsh.dev@gmail.com>
2019-04-08 11:49:39 -0400

    Updates

---
+++ precache-manifest.f87962654c4ba5062ab77ef1e5b6e058.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/sterlingwalsh/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "b1e609d81a4b5f57378e",
+    "url": "/sterlingwalsh/robofriends/static/js/runtime~main.055b6bf6.js"
+  },
+  {
+    "revision": "a52e17cbccf5fe6b516f",
+    "url": "/sterlingwalsh/robofriends/static/js/main.bbecff50.chunk.js"
+  },
+  {
+    "revision": "23c9c903deda4738bb74",
+    "url": "/sterlingwalsh/robofriends/static/js/2.01c51581.chunk.js"
+  },
+  {
+    "revision": "a52e17cbccf5fe6b516f",
+    "url": "/sterlingwalsh/robofriends/static/css/main.8bca3985.chunk.css"
+  },
+  {
+    "revision": "23c9c903deda4738bb74",
+    "url": "/sterlingwalsh/robofriends/static/css/2.35136b2e.chunk.css"
+  },
+  {
+    "revision": "2b243b88fe8e27bfc88592a59f1c3048",
+    "url": "/sterlingwalsh/robofriends/index.html"
+  }
+];
